59d4b7fff21f9f0a5a65481d0ed7018e56556633
ranasobeid95 <ranasobeid95@gmail.com>
2020-11-10 21:02:18 +0200

    add another option for dbURL for test mode

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -5,6 +5,8 @@ let dbURL = '';
 
 if (process.env.NODE_ENV === 'production') {
   dbURL = process.env.DB_CONNECTION;
+} else if (process.env.NODE_ENV === 'test') {
+  dbURL = process.env.TEST_URL;
 } else {
   dbURL = process.env.DEV_URI;
 }
