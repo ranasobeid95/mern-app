c8e112f0ae46baa3643faa95b6698e7db4ab3938
ranasobeid95 <ranasobeid95@gmail.com>
2020-11-13 13:33:13 +0200

    update file path

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const Post = require('../database/models/Posts');
+const Post = require('../database/models/Post');
 
 // Submits a post
 router.post('/', async (req, res) => {
