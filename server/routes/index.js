8f9eaebcf463abf9dc4203e350a06a9a78d1d59e
ranasobeid95 <ranasobeid95@gmail.com>
2020-11-13 13:32:35 +0200

    require auth routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const posts = require('./posts');
+const authRouter = require('./authRoutes');
 
+router.use(authRouter);
 router.use('/posts', posts);
 
 module.exports = router;
