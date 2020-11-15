e08d89f91309c3e99a4f5fc8f80fa9cd77cbfbe8
ranasobeid95 <ranasobeid95@gmail.com>
2020-11-16 00:15:44 +0200

    make protected posts route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const posts = require('./posts');
 const authRouter = require('./authRoutes');
+const { protectedRoute } = require('../controllers/middleware/isAuth');
 
 router.use(authRouter);
-router.use('/posts', posts);
+router.use('/posts', protectedRoute, posts);
 
 module.exports = router;
