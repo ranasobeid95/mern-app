c949aae5a51e41e1ee3a3324a949902a889c7bf2
ranasobeid95 <ranasobeid95@gmail.com>
2020-11-16 00:04:13 +0200

    add errorHandle function to handle auth middleware errors

--- server/controllers/errorHandle.js
+++ server/controllers/errorHandle.js
@@ -1,4 +1,32 @@
 /* eslint-disable no-unused-vars */
+exports.errorHandle = (error) => {
+  const errors = {};
+
+  // duplicate email error
+  if (error.code === 11000) {
+    errors.email = 'that email is already registered';
+    return errors;
+  }
+  // incorrect email
+  if (error.message === 'incorrect email') {
+    errors.email = 'That email is not registered';
+  }
+
+  // incorrect password
+  if (error.message === 'incorrect password') {
+    errors.password = 'That password is incorrect';
+  }
+
+  // validation errors
+  if (error.message.includes('validation failed')) {
+    Object.values(error.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
 exports.clientError = (req, res) => {
   res
     .status(404)
